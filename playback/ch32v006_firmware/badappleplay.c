0488bb63e53c51f506e1fb17e57fc1b9429f35fd
cnlohr <lohr85@gmail.com>
2025-03-08 04:23:42 -0500

    Toying with fixing the bootloader

--- playback/ch32v006_firmware/badappleplay.c
+++ playback/ch32v006_firmware/badappleplay.c
@@ -1,22 +1,8 @@
+/* Small example showing how to use the SWIO programming pin to 
+   do printf through the debug interface */
+
 #include "ch32fun.h"
 #include <stdio.h>
-
-
-#define WARNING(x...)
-
-#define BADATA_DECORATOR const __attribute__((section(".fixedflash")))
-#define BAS_DECORATOR    const __attribute__((section(".fixedflash")))
-
-#include "ba_play.h"
-#include "ba_play_audio.h"
-
-uint8_t out_buffer_data[AUDIO_BUFFER_SIZE];
-ba_play_context ctx;
-
-volatile uint32_t kas = 0;
-
-////////////////////////////////////////////////////////////////////////////////////////////
-
 
 #define SSD1306_CUSTOM_INIT_ARRAY 1
 #define SSD1306_I2C_ADDR 0x3C
@@ -27,68 +13,131 @@ volatile uint32_t kas = 0;
 
 #define SSD1306_RST_PIN PC3
 
-#define I2CDELAY_FUNC( x )
-
+//#define I2CDELAY_FUNC( x )
 #include "ssd1306mini.h"
+#include "ssd1306.h"
 
-static const uint8_t ssd1306_init_array[] =
-{
-	0xAE, // Display off
-	0x20, 0x00, // Horizontal addresing mode
-	0x00, 0x12, 0x40, 0xB0,
-	0xD5, 0xf0, // Function Selection   <<< This controls scan speed. F0 is fastest.
-	0xA8, 0x2F, // Set Multiplex Ratio
-	0xD3, 0x00, // Set Display Offset
-	0x40,
-	0xA1, // Segment remap
-	0xC8, // Set COM output scan direction
-	0xDA, 0x12, // Set COM pins hardware configuration
-	0x81, 0xaF, // Contrast control (0xCF is very bright)
-	//0xD9, 0x22, // Set Pre-Charge Period  (Not used)
-	0xDB, 0x30, // Set VCOMH Deselect Level
-//	0xA4, // Entire display on (a5)/off(a4)
-	0xA6, // Normal (a6)/inverse (a7)
-//	0x8D, 0x14, // Set Charge Pump //XXX TODO CHECK ME FIRST.
-	0xAF, // Display On
-	SSD1306_COLUMNADDR, SSD1306_OFFSET, SSD1306_OFFSET+SSD1306_W-1,
-	SSD1306_PAGEADDR, 0, 7, // Page setup, start at 0 and end at 7
-};
 
-uint8_t ssd1306_buffer[64];
+#define RAINDROPS 1
+// If not defined, does a grey test.
 
-////////////////////////////////////////////////////////////////////////////////////////////
+#define RANDOM_STRENGTH 2
+#include "lib_rand.h"
+
+
+	__attribute__((section(".text")))  const uint8_t ssd1306_init_array[] =
+	{
+		0xAE, // Display off
+		0x20, 0x00, // Horizontal addresing mode
+		0x00, 0x12, 0x40, 0xB0,
+		0xD5, 0xf0, // Function Selection   <<< This controls scan speed. F0 is fastest.  The LSN = D divisor.
+		0xA8, 0x2F, // Set Multiplex Ratio
+		0xD3, 0x00, // Set Display Offset
+		0x40,
+		0xA1, // Segment remap
+		0xC8, // Set COM output scan direction
+		0xDA, 0x12, // Set COM pins hardware configuration
+		0x81, 0xcf, // Contrast control
+		//0xD9, 0x22, // Set Pre-Charge Period  (Not used)
+		0xDB, 0x30, // Set VCOMH Deselect Level
+		0xA4, // Entire display on (a5)/off(a4)
+		0xA6, // Normal (a6)/inverse (a7)
+		0x8D, 0x14, // Set Charge Pump
+		0xAF, // Display On
+		SSD1306_PAGEADDR, 0, 7, // Page setup, start at 0 and end at 7
+	};
+
+__attribute__((section(".text"))) const uint8_t framestart[] = {0xD3, 0x32, 0xA8, 0x01,
+			// Column start address (0 = reset)
+			// Column end address (127 = reset)
+			SSD1306_COLUMNADDR, SSD1306_OFFSET, SSD1306_OFFSET+SSD1306_W-1, 0xb0 };
+
+__attribute__((section(".text"))) const uint8_t frameend[] = {0xD3, 0x3e, 0xA8, 0x31};
 
 int main()
 {
-	funGpioInitAll();
 	SystemInit();
+	funGpioInitAll();
+
 	ssd1306_mini_i2c_setup();
+
 
 	// Trying another mode
 	ssd1306_mini_pkt_send( ssd1306_init_array, sizeof(ssd1306_init_array), 1 );
 
-	ba_play_setup( &ctx );
-	ba_audio_setup();
+	//int x, y;
+	int t = 0;
 
-	int lasttail = 0;
-	int outbuffertail = 0;
-	int frame = 0;
 
 	int32_t nextFrame = SysTick->CNT;
 	uint32_t freeTime;
 
-	int subframe = 0;
+	#define DROPCOUNT 16
+	static int drops[DROPCOUNT][3]; // x,y,age
+	static int drophead = 0;
+
+	int totalcircles = 0;
 
 	while(1)
 	{
-		if( subframe == 3 )
+		#define profile 0
+
+		uint32_t sz = sizeof( ssd1306_buffer )/4;
+		uint32_t i;
+
+#if RAINDROPS
+
+		for( i = 0; i < sz; i++ )
 		{
-			if( frame == FRAMECT ) asm volatile( "j 0" );
-			ba_play_frame( &ctx );
-			lasttail = outbuffertail;
-			subframe = 0;
-			frame++;
+			((uint32_t*)ssd1306_buffer)[i] = 0;
 		}
+
+		if( !profile || totalcircles < 8 )
+		{
+			if( (t & 0xf) == (rand()%0xf) )
+			{
+				drophead = (drophead+1)&(DROPCOUNT-1);
+				int * dr = drops[drophead];
+				dr[0] = rand()%64;
+				dr[1] = rand()%48;
+				dr[2] = 1;
+				totalcircles++;
+			}
+
+			int d;
+			for( d = 0; d < DROPCOUNT; d++ )
+			{
+				int * dr = drops[d];
+				if( !dr[2] ) continue;
+				dr[2]++;
+				if( dr[2] > 80 ) dr[2] = 0;
+			}
+		}
+		if( profile )
+		{
+			char st[12];
+			mini_itoa( freeTime, 10, 0, 1, st );
+			ssd1306_drawstr( 0, 0, st, 1);
+		}
+		//uint32_t profile_start = SysTick->CNT;
+		for( int d = 0; d < DROPCOUNT; d++ )
+		{
+			int * dr = drops[d];
+			if( !dr[2] ) continue;
+			ssd1306_drawCircle( dr[0], dr[1], dr[2], 1 );
+
+			//ssd1306_fillRect( dr[0]-dr[2], dr[1]-dr[2], dr[2]*2, dr[2]*2, 1 );
+			//ssd1306_drawLine( dr[0], dr[1], dr[0]-dr[2], dr[1]-dr[2]/2, 1 );
+		}
+
+
+		//freeTime = SysTick->CNT - profile_start;
+
+		//memset( ssd1306_buffer, ((t+x+y)&1)?0xaa:0x55, sizeof( ssd1306_buffer ) );
+#else
+#endif
+
+		t++;
 
 		freeTime = nextFrame - SysTick->CNT;
 		while( (int32_t)(SysTick->CNT - nextFrame) < 0 );
@@ -97,67 +146,41 @@ int main()
 		// 800000 is 60Hz
 		// 533333 is 90Hz -- 90Hz seems about the max you can go with default 0xd5 settings.
 		// 400000 is 120Hz -- Only possible when cranking D5 at 0xF0 and 0xa8 == 0x31
+		// 320000 is 150Hz -- A biiig stretch. but on some displays achievable.
+		// 266666 is 180Hz -- DOES NOT WORK
 
 		// Move the cursor "off screen"
 		// Scan over two scanlines to hide updates
+
 		ssd1306_mini_pkt_send( 
-			(const uint8_t[]){0xD3, 0x32, 0xA8, 0x01,
-			// Column start address (0 = reset)
-			// Column end address (127 = reset)
-			}, 4, 1 );
+			framestart,
+			8, 1 );
 
-		// Has to be a different transaction for some reason.
-		ssd1306_mini_pkt_send( (const uint8_t[]){0xb0}, 1, 1 );
 
+		if( t == 2 ) t = 0;
+#if RAINDROPS
+		// May be needed at lower framerates.
+		Delay_Us(200);
 
 		// Send data
-		int y;
-
+		ssd1306_mini_data(ssd1306_buffer, 64*48/8);
+#else
 		ssd1306_mini_i2c_sendstart();
 		ssd1306_mini_i2c_sendbyte( SSD1306_I2C_ADDR<<1 );
 		ssd1306_mini_i2c_sendbyte( 0x40 ); // Data
 
-		glyphtype * gm = ctx.curmap;
-		for( y = 0; y < 6; y++ )
-		{
-			int x;
-			for( x = 0; x < 8; x++ )
-			{
-				glyphtype gindex = *(gm++);
-				graphictype * g = ctx.glyphdata[gindex];
-				
-				int lg;
-				for( lg = 0; lg< 8; lg ++ )
-				{
-					int go = g[lg];
-					if( (subframe+x+y)&3 )
-						go >>= 8;
-					ssd1306_mini_i2c_sendbyte( go );
-				}
-			}
-			//memset( ssd1306_buffer, n, sizeof( ssd1306_buffer ) );
-			//int k;
-			//for( k = 0; k < sizeof(ssd1306_buffer); k++ ) ssd1306_buffer[k] = frame;
-			//ssd1306_mini_data(ssd1306_buffer, sizeof(ssd1306_buffer));
-		}
+		Delay_Us(100);
+		int n;
+		uint8_t go = (t == 0 || t == 2) ? 0xff : 0x00;
+		for( n = 0; n < 6*8*8; n++ )
+			ssd1306_mini_i2c_sendbyte( go );
 
 		ssd1306_mini_i2c_sendstop();
+#endif
 
 		// Make it so it "would be" off screen but only by 2 pixels.
 		// Overscan screen by 2 pixels, but release from 2-scanline mode.
-		ssd1306_mini_pkt_send( (const uint8_t[]){0xD3, 0x3e, 0xA8, 0x31}, 4, 1 ); 
-
-		outbuffertail = (F_SPS/30*frame) % AUDIO_BUFFER_SIZE;
-		ba_audio_fill_buffer( out_buffer_data, outbuffertail );
-
-		for( int n = lasttail; n != outbuffertail; n = (n+1)%AUDIO_BUFFER_SIZE )
-		{
-			//float fo = out_buffer_data[n] / 128.0 - 1.0;
-			//fwrite( &fo, 1, 4, fAudioDump );
-			// Do something with out_buffer_data.
-		}
-
-		subframe++;
+		ssd1306_mini_pkt_send( (const uint8_t[]){0xD3, 0x3e, 0xA8, 0x31}, 4, 1 );  // There are other conditions where this is better if it's 0xA8 0x31.
 	}
 }
 
